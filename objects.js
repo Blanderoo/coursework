94e8dfc6f4bbd5e0ca2ddaaef60f1d981d188880
Blanca <141976172+Blanderoo@users.noreply.github.com>
2023-10-05 17:28:39 -0600

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,15 @@
+//create a show object with several properties
+
+const show = {
+    brand: "converse",
+    laces: true,
+    velcro: false,
+    price: 75.00,
+    activityType: "walking",
+};
+
+//console table show
+console.table(shoe)
+
+console.log(shoe.brand);
+console.log(shoe  ["brand"]);
